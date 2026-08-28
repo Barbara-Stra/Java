0f86b6527a7d9f810ad646087ad9dcd07b857d87
Barbara-Stra <babistra1508@gmail.com>
2026-08-28 15:08:35 -0300

    Delete exercicio 02 (construtores, super) directory

--- exercicio 02 (construtores, super)/src/ProdutoPerecivel.java
+++
@@ -1,9 +0,0 @@
-public class ProdutoPerecivel extends Produto {
-    private String dataValidade;
-
-    public ProdutoPerecivel(String nome, double preco, int quantidade, String dataValidade){
-        super(nome, preco, quantidade);
-        this.dataValidade = dataValidade;
-    }
-
-}
